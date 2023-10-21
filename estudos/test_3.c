4c733df90ccce0d47816080009af96a6fab42d43
Angelicahaas <211061832@aluno.unb.br>
2023-10-21 17:46:40 -0300

    playlist de estudos

--- estudos/test_3.c
+++ estudos/test_3.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    int a = 10;
+    int *b = &a;
+    int **c = &b;        // poteiro para uma variavel do tipo int *
+
+    printf("&a = %p, a =  %d\n", &a, a);
+    printf("&b = %p, b =  %p, *b = %d\n", &b, b, *b); // %p = endereço
+    printf("&c = %p, c =  %p, *c = %p, **c = %d\n\n", &c, c, *c, **c);
     
 
+    **c = 99;
+
+    printf("&a = %p, a =  %d\n", &a, a);
+    printf("&b = %p, b =  %p, *b = %d\n", &b, b, *b); // %p = endereço
+    printf("&c = %p, c =  %p, *c = %p, **c = %d\n\n", &c, c, *c, **c);
+    
 
     return 0;
 }
